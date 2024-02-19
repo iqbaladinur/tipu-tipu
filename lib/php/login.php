ee1a7267bca222eb8a6f77e21e67f8d81fb3fa35
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2024-02-19 09:30:51 +0700

    Update login.php

--- lib/php/login.php
+++ lib/php/login.php
@@ -16,6 +16,7 @@
 		*/
 		$sql="SELECT * FROM user_id WHERE id_usr='$user' OR e_mail='$user'";
 		$result=mysql_query($sql);
+		var_dump($sql);
 		var_dump(mysql_num_rows($result));
 		// if (mysql_num_rows($result) == 1) { //cek sql result
 		// 	$data_user=mysql_fetch_array($result);
